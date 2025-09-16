d202b46801aa3eeb8dcd6f66121d12e91a66c379
PUNNAM PAVAN KUMAR <pavankumar111205@gmail.com>
2025-09-16 14:32:27 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 import os
 from flask import Flask
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 from dotenv import load_dotenv
 from flask_login import LoginManager
-import cloudinary # Import cloudinary
+import cloudinary
+import certifi # Import the certifi library
 
 load_dotenv()
 
 MONGO_URI = os.getenv('MONGO_URI')
-client = MongoClient(MONGO_URI)
+
+# --- THIS IS THE CORRECTED DATABASE CONNECTION ---
+# We are telling MongoClient to use certifi's certificates for a secure connection
+client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client.get_database('student_companion_db')
+# --- END OF CORRECTION ---
 
 login_manager = LoginManager()
 
@@ -19,11 +23,19 @@ def create_app():
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'a_default_secret_key_for_development')
     app.config['ADMIN_EMAIL'] = os.getenv('ADMIN_EMAIL')
 
+    # Cloudinary Config
+    cloudinary.config(
+        cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
+        api_key = os.getenv('CLOUDINARY_API_KEY'),
+        api_secret = os.getenv('CLOUDINARY_API_SECRET'),
+        secure = True
+    )
 
     login_manager.init_app(app)
     login_manager.login_view = 'main.login'
 
     from .models import User
+    from bson.objectid import ObjectId
 
     @login_manager.user_loader
     def load_user(user_id):
